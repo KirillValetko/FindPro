fb3a7674a5c80ef9f3bd2a240da4cd53d0c37ffe
KirillValetko <kirya_valet@mail.ru>
2024-03-31 21:15:44 +0300

    Added skill group service

---
+++ FindPro.BLL/Models/SkillLevelModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FindPro.BLL.Models
+{
+    public class SkillLevelModel : BaseModel
+    {
+        public string LevelName { get; set; }
+        public int LevelValue { get; set; }
+        public string Description { get; set; }
+        public bool IsUsed { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
